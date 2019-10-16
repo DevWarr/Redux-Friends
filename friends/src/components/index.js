3a786665662913db69efc15209f6ea9029c645e6
Devin Warrick <dev.warr@gmail.com>
2019-10-15 19:49:54 -0500

    Remove all

--- friends/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as FriendList } from "./FriendList/FriendList";
-export { default as FriendItem } from "./FriendItem/FriendItem";
-export { default as Login } from "./Login/Login";
-export { default as NavBar } from "./NavBar/NavBar";
-export { default as NewFriendForm } from "./NewFriendForm/NewFriendForm";
