9171f58133a9122f621364749f0779a13c1b5c21
Devin Warrick <dev.warr@gmail.com>
2019-10-15 22:48:47 -0500

    Add comments through code

--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -2,27 +2,46 @@ import React from "react";
 import axiosWithAuth from "../utils/axiosWithAuth";
 
 export default function FriendForm(props) {
+    
+  // Creating our state, and our handleChanges function
   const [form, setForm] = React.useState({ name: "", age: "", email: "" });
+  const handleChanges = e => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
 
+  // IF our editingFriend changes, we call useEffect:
   React.useEffect(() => {
+
+    // If editingFriend is true -> We do have someone we want to edit, 
+    // so setForm() with all of our current values so we can update them.
     if (props.editingFriend) {
       setForm({
         name: props.editingFriend.name,
         age: props.editingFriend.age,
         email: props.editingFriend.email
       });
+
+    // If editingFriend is not true -> We don't have anyone we want to edit,
+    // so reset the form.
     } else {
       setForm({ name: "", age: "", email: "" });
     }
-  }, [props.editingFriend]);
+  }, [props.editingFriend]); // Listening for props.editingFriend
 
-  const handleChanges = e => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
 
+
+  /**
+   * When we submit our Form successfully, two things could happen.
+   * - If we're editing a friend, we call a `PUT` request
+   * - If we're simply adding a friend, we call a `POST` request
+   * 
+   * On a successful request, we always update our state, reset our form,
+   * and set our EditingFriend to `null` so the code knows we're no longer editing.
+   */
   const submitHandler = e => {
     e.preventDefault();
 
+    // If editingFriend is true -> We are editing a friend, so we want a PUT request
     if (props.editingFriend) {
       axiosWithAuth()
         .put(`/api/friends/${props.editingFriend.id}`, form)
@@ -32,6 +51,8 @@ export default function FriendForm(props) {
           setForm({ name: "", age: "", email: "" });
           props.setEditingFriend(null);
         });
+
+    // If editingFriend is not true -> We aren't editing a friend, so we want a POST request
     } else {
       axiosWithAuth()
         .post("/api/friends", form)
@@ -44,6 +65,7 @@ export default function FriendForm(props) {
     }
   };
 
+  // Set's editingFriend to null to tell the code that we are no longer editing
   const closeEdit = e => {
     e.preventDefault();
     props.setEditingFriend(null);
